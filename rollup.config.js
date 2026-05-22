c553d961fc92560bacbb94c865e397f71a1a7b86
David Beall <6121439+beallio@users.noreply.github.com>
2026-05-21 22:36:48 -0700

    fix(rollup): configure loader and treeshaking to bundle static CSS

--- rollup.config.js
+++ rollup.config.js
@@ -4,19 +4,19 @@ function cssInjectedPlugin() {
   return {
     name: 'css-injected-plugin',
     transform(code, id) {
-      if (id.endsWith('.css')) {
+      if (/\.css(?:\?.*)?$/.test(id)) {
         const escapedCss = code.replace(/\\/g, '\\\\').replace(/`/g, '\\`').replace(/\$/g, '\\$');
         return {
           code: `
             if (typeof window !== 'undefined' && typeof document !== 'undefined') {
               const style = document.createElement('style');
-              style.id = 'sdh-ludusavi-styles';
               style.textContent = \`${escapedCss}\`;
               document.head.appendChild(style);
             }
             export default \`${escapedCss}\`;
           `,
-          map: { mappings: '' }
+          map: { mappings: '' },
+          moduleSideEffects: true
         };
       }
     }
