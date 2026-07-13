d1f33f02c92e58b41dea29ec73c2450101cd7974
David Beall <6121439+beallio@users.noreply.github.com>
2026-07-12 18:40:37 -0700

    chore: clean up scratch scripts

--- fix_newlines.py
+++
@@ -1,7 +0,0 @@
-with open("src/controllers/gameLifecycleController.tsx", "r") as f:
-    content = f.read()
-
-content = content.replace("\\n", "\n")
-
-with open("src/controllers/gameLifecycleController.tsx", "w") as f:
-    f.write(content)
